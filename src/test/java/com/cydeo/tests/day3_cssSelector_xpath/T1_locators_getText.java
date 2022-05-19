51c1317516d24d87f70c30b21720cce2a2ea525a
cholponai <cholpondzhumabaeva@gmail.com>
2022-05-19 08:29:13 +0200

    day3: Task1_locators_getText_getAttribute

--- src/test/java/com/cydeo/tests/day3_cssSelector_xpath/T1_locators_getText.java
+++ src/test/java/com/cydeo/tests/day3_cssSelector_xpath/T1_locators_getText.java
@@ -1,5 +1,6 @@
 package com.cydeo.tests.day3_cssSelector_xpath;
 
+import com.cydeo.utilities.WebDriverFactory;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -11,8 +12,10 @@ public class T1_locators_getText {
     public static void main(String[] args) {
         // TC #1: NextBaseCRM, locators and getText() practice
         //1- Open a chrome browser
-        WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
+        //WebDriverManager.chromedriver().setup();
+        //WebDriver driver = new ChromeDriver();
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
+
         driver.manage().window().maximize();
 
         //2- Go to: https://login1.nextbasecrm.com/
@@ -34,6 +37,16 @@ public class T1_locators_getText {
 
         //6- Verify error message text is as expected:
         //Expected: Incorrect login or password
+        WebElement errorMessage = driver.findElement(By.className("errortext"));
+        String expectedErrorMessage = "Incorrect login or password";
+        String actualErrorMessage = errorMessage.getText();
+
+        if (actualErrorMessage.equals(expectedErrorMessage)){
+            System.out.println("Error message verification PASSED!");
+        }else {
+            System.out.println("Error message verification FAILED!!!");
+        }
+
 
     }
 }
