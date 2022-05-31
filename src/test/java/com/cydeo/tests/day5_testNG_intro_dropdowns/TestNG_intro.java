ac3850c06b08a119ebf449ebe017f5bc43b2496e
cholponai <cholpondzhumabaeva@gmail.com>
2022-05-31 11:25:15 +0200

    day5: TestNG_intro

--- src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/TestNG_intro.java
+++ src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/TestNG_intro.java
@@ -1,23 +1,48 @@
 package com.cydeo.tests.day5_testNG_intro_dropdowns;
 
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
+import org.testng.Assert;
+import org.testng.annotations.*;
 
 public class TestNG_intro {
+
+    @BeforeClass
+    public void setupMethod(){
+        System.out.println("---> BeforeClass is running");
+    };
+
+    @AfterClass
+    public void tearDownClass(){
+        System.out.println("----> AfterClass is running");
+    }
 
     @BeforeMethod
     public void setUpMethod(){
         System.out.println("----> BeforeMethod is running!");
     }
 
-    @Test
-    public void test1(){
-        System.out.println("Test 1 is running...");
+    @AfterMethod
+    public void tearDownMethod(){
+        System.out.println("---->AfterMethod is running");
     }
 
-    @Test
+    @Test(priority = 2)
+    public void test1(){
+        System.out.println("Test 1 is running...");
+        //ASSERT EQUALS: Compare 2 of the same things
+        String actual ="apple";
+        String expected = "apple2";
+        Assert.assertEquals(actual,expected);
+    }
+
+
+
+    @Test(priority = 1)
     public void test2(){
         System.out.println("Test 2 is running...");
+
+        String actual ="apple";
+        String expected = "apple2";
+        Assert.assertTrue(actual.equals(expected)       );
     }
 
 }
