882f19946510b6c38b3597a925678b20be9d241e
cholponai <cholpondzhumabaeva@gmail.com>
2022-05-31 09:10:20 +0200

    day5: TestNG_intro

---
+++ src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/TestNG_intro.java
@@ -0,0 +1,11 @@
+package com.cydeo.tests.day5_testNG_intro_dropdowns;
+
+import org.testng.annotations.Test;
+
+public class TestNG_intro {
+
+    @Test
+    public void test1(){
+        System.out.println("Test 1 is running...");
+    }
+}
