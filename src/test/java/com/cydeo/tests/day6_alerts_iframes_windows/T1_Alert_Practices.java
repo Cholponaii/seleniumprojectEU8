93b2c9f448b4907de2ebabe5cf27f269faa32466
cholponai <cholpondzhumabaeva@gmail.com>
2022-06-02 11:35:10 +0200

    day6_alert_Practices

--- src/test/java/com/cydeo/tests/day6_alerts_iframes_windows/T1_Alert_Practices.java
+++ src/test/java/com/cydeo/tests/day6_alerts_iframes_windows/T1_Alert_Practices.java
@@ -1,8 +1,13 @@
 package com.cydeo.tests.day6_alerts_iframes_windows;
 
 import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
 
@@ -19,8 +24,34 @@ public class T1_Alert_Practices {
 
     }
 
+    @Test
+    public void alert_test1() throws InterruptedException {
 
-    //3. Click to “Click for JS Alert” button
-    //4. Click to OK button from the alert
-    //5. Verify “You successfully clicked an alert” text is displayed.
+        //3. Click to “Click for JS Alert” button
+        WebElement informationAlertButton = driver.findElement(By.xpath("//button[.='Click for JS Alert']"));
+        informationAlertButton.click();
+        Thread.sleep(2000);
+
+        //To be able to click to Alert OK button we need to switch driver's focus to Alert itself.
+        Alert alert = driver.switchTo().alert();
+
+        //4. Click to OK button from the alert
+        alert.accept();
+
+        //5. Verify “You successfully clicked an alert” text is displayed.
+        WebElement resultText = driver.findElement(By.xpath("//p[@id='result']"));
+
+        //Failure message will only be displayed if assertion fails: "Result text is NOT displayed."
+        Assert.assertTrue(resultText.isDisplayed(), "Result Text is NOT displayed ");
+
+        String expectedText = "You successfully clicked an alert";
+        String actualText = resultText.getText();
+
+        Assert.assertEquals(actualText,expectedText,"Actual result Text is NOT as expected!!!");
+
+
+    }
+
+
+
 }
