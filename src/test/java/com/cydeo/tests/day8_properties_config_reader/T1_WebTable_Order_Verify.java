63b01689c7a49d6d67bfb402b770eb9e4964f087
cholponai <cholpondzhumabaeva@gmail.com>
2022-06-07 14:30:51 +0200

    day8 T1_WebTable_Order_Verify

--- src/test/java/com/cydeo/tests/day8_properties_config_reader/T1_WebTable_Order_Verify.java
+++ src/test/java/com/cydeo/tests/day8_properties_config_reader/T1_WebTable_Order_Verify.java
@@ -1,6 +1,7 @@
 package com.cydeo.tests.day8_properties_config_reader;
 
 import com.cydeo.utilities.WebDriverFactory;
+import com.cydeo.utilities.WebTableUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -29,17 +30,39 @@ public class T1_WebTable_Order_Verify {
         System.out.println("bobMartinCell.getText() = " + bobMartinCell.getText());
 
         //2. Verify Bob’s name is listed as expected.
+        //Expected: “Bob Martin
         String expectedBobName= "Bob Martin";
         String actualBobName= bobMartinCell.getText();
         Assert. assertEquals(actualBobName,expectedBobName);
 
-        //Expected: “Bob Martin”
+
         //3. Verify Bob Martin’s order date is as expected
         //Expected: 12/31/2021
+
+        // alternative locator to bobMartinDateCell = "//td[.='Bob Martin']/../td[5]"
         WebElement bobMartinDateCell= driver.findElement(By.xpath("//table[@id='ctl00_MainContent_orderGrid']/tbody//td[.='Bob Martin']/following-sibling::td[3]"));
 
         String expectedBobDate= "12/31/2021";
         String actualBobDate= bobMartinDateCell.getText();
         Assert.assertEquals(actualBobDate,expectedBobDate);
     }
+
+        //We use the utility method we created.
+        @Test
+        public void test2(){
+
+        String customerOrderDate1 = WebTableUtils.returnOrderDate(driver,"Aleksandra Gray");
+            System.out.println("customerOrderDate1 = " + customerOrderDate1);
+
+            String customerOrderDate2 = WebTableUtils.returnOrderDate(driver,"John Doe");
+            System.out.println("customerOrderDate2 = " + customerOrderDate2);
+    }
+
+    //Using WebTableUtils.orderVerify(); method
+    @Test
+    public void test3(){
+        WebTableUtils.orderVerify(driver,"Bart Fisher","01/16/2021");
+
+    }
+
 }
