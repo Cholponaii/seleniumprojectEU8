4335f277b62896a2978f708e1e076e445d956a1b
cholponai <cholpondzhumabaeva@gmail.com>
2022-06-11 15:48:02 +0200

    day9 Driver.java

--- src/test/java/com/cydeo/utilities/Driver.java
+++ src/test/java/com/cydeo/utilities/Driver.java
@@ -3,6 +3,7 @@ package com.cydeo.utilities;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.util.concurrent.TimeUnit;
 
@@ -45,7 +46,12 @@ public class Driver {
                     driver= new ChromeDriver();
                     driver.manage().window().maximize();
                     driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-
+                    break;
+                case "firefox":
+                    WebDriverManager.firefoxdriver().setup();
+                    driver= new FirefoxDriver();
+                    driver.manage().window().maximize();
+                    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
             }
 
         }
