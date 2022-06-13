8063dce61c006f9ce2dc8f2ad78f1481baa0969d
cholponai <cholpondzhumabaeva@gmail.com>
2022-06-13 11:37:11 +0200

    day10 T1_Registration_Form.java

--- src/test/java/com/cydeo/tests/day10_upload_actions_jsexecuter/T1_Registration_Form.java
+++ src/test/java/com/cydeo/tests/day10_upload_actions_jsexecuter/T1_Registration_Form.java
@@ -1,26 +1,60 @@
 package com.cydeo.tests.day10_upload_actions_jsexecuter;
 
+import com.cydeo.utilities.ConfigurationReader;
+import com.cydeo.utilities.Driver;
+import com.github.javafaker.Faker;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
 public class T1_Registration_Form {
 
-    //
-    //TC#1: Registration form confirmation
-    //Note: Use JavaFaker OR read from configuration.properties file when possible.
-    //1. Open browser
-    //2. Go to website: https://practice.cydeo.com/registration_form
-    //3. Enter first name
-    //4. Enter last name
-    //5. Enter username
-    //6. Enter email address
-    //7. Enter password
-    //8. Enter phone number
-    //9. Select a gender from radio buttons
-    //10. Enter date of birth
-    //11. Select Department/Office
-    //12. Select Job Title
-    //13. Select programming language from checkboxes
-    //14. Click to sign up button
-    //15. Verify success message “You’ve successfully completed registration.” is
-    //displayed.
+    @Test
+    public void registration_form_test(){
+
+        //
+        //TC#1: Registration form confirmation
+        //Note: Use JavaFaker OR read from configuration.properties file when possible.
+        //1. Open browser
+        //2. Go to website: https://practice.cydeo.com/registration_form
+        //Driver.getDriver()-->driver
+        Driver.getDriver().get(ConfigurationReader.getProperty("Registration.form.url"));
+
+        //create JavaFaker object
+        Faker faker = new Faker();
+
+        //3. Enter first name
+        WebElement inputFirstName = Driver.getDriver().findElement(By.xpath("//input[@name='firstname']"));
+        inputFirstName.sendKeys(faker.name().firstName());
+        //4. Enter last name
+        WebElement inputLastname = Driver.getDriver().findElement(By.xpath("//input[@name='lastname']"));
+        inputLastname.sendKeys(faker.name().lastName());
+        //5. Enter username
+        WebElement inputUsername = Driver.getDriver().findElement(By.xpath("//input[@name='username']"));
+        //inputUsername.sendKeys(faker.name().username().replaceAll(".", ""));
+        String user = (faker.bothify("helpdesk###"));
+        inputUsername.sendKeys(user);
+
+        //6. Enter email address
+        WebElement inputEmail = Driver.getDriver().findElement(By.xpath("//input[@name='email']"));
+        inputEmail.sendKeys(user+"@mail.com");
+
+
+
+        //7. Enter password
+        //8. Enter phone number
+        //9. Select a gender from radio buttons
+        //10. Enter date of birth
+        //11. Select Department/Office
+        //12. Select Job Title
+        //13. Select programming language from checkboxes
+        //14. Click to sign up button
+        //15. Verify success message “You’ve successfully completed registration.” is
+        //displayed.
+
+    }
+
+
 
 
 }
