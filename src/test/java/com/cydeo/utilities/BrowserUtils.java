ae8ea8d94dc4b8d21ad7ba8ec7b5e15c8d0a35f1
cholponai <cholpondzhumabaeva@gmail.com>
2022-06-13 13:56:58 +0200

    day10 T2_Upload_Practice.java

--- src/test/java/com/cydeo/utilities/BrowserUtils.java
+++ src/test/java/com/cydeo/utilities/BrowserUtils.java
@@ -11,7 +11,7 @@ public class BrowserUtils {
 In this class only general utility methods that are NOT related to some specific page.
  */
 
-    public void sleep(int second){
+    public static void sleep(int second){
         second*=1000;
 
         try {
