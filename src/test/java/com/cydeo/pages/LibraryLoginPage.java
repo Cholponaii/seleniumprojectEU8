abeb3e03f4e6523d041eccb7fa05a480c5cec424
cholponai <cholpondzhumabaeva@gmail.com>
2022-06-18 11:33:53 +0200

    day12 LibraryLoginPage.java

--- src/test/java/com/cydeo/pages/LibraryLoginPage.java
+++ src/test/java/com/cydeo/pages/LibraryLoginPage.java
@@ -1,6 +1,8 @@
 package com.cydeo.pages;
 
 import com.cydeo.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class LibraryLoginPage {
@@ -9,4 +11,24 @@ public class LibraryLoginPage {
     public LibraryLoginPage(){
         PageFactory.initElements(Driver.getDriver(),this);
     }
+
+    //#2- use @FindBy annotation to locate web elements
+
+    @FindBy(xpath ="//input[@id='inputEmail']" )
+    public WebElement inputUsername;
+
+    @FindBy(id ="inputPassword")
+    public WebElement inputPassword;
+
+    @FindBy(xpath = "//button[text()='Sign in']")
+    public WebElement signInButton;
+
+    @FindBy(xpath = "//div[@id='inputEmail-error']")
+    public WebElement fieldRequiredMessage;
+
+    @FindBy(xpath = "//div[.= 'Please enter a valid email address.']")
+    public WebElement enterValidEmailErrorMessage;
+
+    @FindBy(xpath = "//div[text()='Sorry, Wrong Email or Password']")
+    public WebElement wrongEmailOrPasswordErrorMessage;
 }
