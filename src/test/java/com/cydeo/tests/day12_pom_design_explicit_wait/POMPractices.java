8713f51cf44151174accc95ad67d1c74d447eca1
cholponai <cholpondzhumabaeva@gmail.com>
2022-06-20 09:55:07 +0200

    day12 DynamicLoad7Page.java

--- src/test/java/com/cydeo/tests/day12_pom_design_explicit_wait/POMPractices.java
+++ src/test/java/com/cydeo/tests/day12_pom_design_explicit_wait/POMPractices.java
@@ -4,20 +4,29 @@ import com.cydeo.pages.LibraryLoginPage;
 import com.cydeo.utilities.Driver;
 import net.bytebuddy.asm.Advice;
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class POMPractices {
     LibraryLoginPage libraryLoginPage;
+
+    @BeforeMethod
+    public void setupMethod(){
+
+        Driver.getDriver().get("https://library1.cydeo.com");
+        libraryLoginPage = new LibraryLoginPage();
+
+    }
 
     @Test
     public void required_field_error_message_test(){
         //TC #1: Required field error message test
         //1- Open a chrome browser
         //2- Go to: https://library1.cydeo.com
-        Driver.getDriver().get("https://library1.cydeo.com");
+
         //3- Do not enter any information
         //4- Click to “Sign in” button
-        libraryLoginPage = new LibraryLoginPage();
+
         libraryLoginPage.signInButton.click();
 
         //5- Verify expected error is displayed:
@@ -32,10 +41,9 @@ public class POMPractices {
         //TC #2: Invalid email format error message test
         //1- Open a chrome browser
         //2- Go to: https://library1.cydeo.com
-        Driver.getDriver().get("https://library1.cydeo.com");
+
 
         //3- Enter invalid email format
-        libraryLoginPage = new LibraryLoginPage();
         libraryLoginPage.inputUsername.sendKeys("uhksjnls");
         libraryLoginPage.signInButton.click();
 
@@ -51,13 +59,9 @@ public class POMPractices {
         //TC #3: Library negative login
         //1- Open a chrome browser
         //2- Go to: https://library1.cydeo.com
-        Driver.getDriver().get("https://library1.cydeo.com");
-
         //3- Enter incorrect username or incorrect password
-        libraryLoginPage = new LibraryLoginPage();
         libraryLoginPage.inputUsername.sendKeys("wrong@username.com");
         libraryLoginPage.inputPassword.sendKeys("wrongpassword");
-
 
         libraryLoginPage.signInButton.click();
 
@@ -65,5 +69,7 @@ public class POMPractices {
         //Expected: Sorry, Wrong Email or Password
 
         Assert.assertTrue(libraryLoginPage.wrongEmailOrPasswordErrorMessage.isDisplayed());
+
+        Driver.closeDriver();
     }
 }
